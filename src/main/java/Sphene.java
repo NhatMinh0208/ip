af8dcdaf7bed72103bcf25d6f185f4bae277a64f
NhatMinh0208 <minhkhicon2468@gmail.com>
2025-01-17 21:14:28 +0800

    Rename bot to Sphene

--- src/main/java/Sphene.java
+++ src/main/java/Sphene.java
@@ -1,6 +1,6 @@
-public class Merylwb {
+public class Sphene {
     public static void main(String[] args) {
-        String name = "Merylwb";
+        String name = "Sphene";
         System.out.println("Hello! I'm " + name + "!");
         System.out.println("How can I help you today?");
         System.out.println("Bye. Hope to see you again soon!");
