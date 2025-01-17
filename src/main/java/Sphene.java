df4a606cd294c2fb662fb330cfef6b8d22694bd2
NhatMinh0208 <minhkhicon2468@gmail.com>
2025-01-17 21:42:26 +0800

    Implement echo

--- src/main/java/Sphene.java
+++ src/main/java/Sphene.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Sphene {
+    private static final String CMD_EXIT = "bye";
     public static void main(String[] args) {
         String name = "Sphene";
+
         System.out.println("Hello! I'm " + name + "!");
-        System.out.println("How can I help you today?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("How can I serve you today, my precious citizen?");
+
+        // Read and evaluate command loop
+        while (true) {
+            Scanner stdin = new Scanner(System.in);
+            String command = stdin.nextLine();
+            if (command.equals(CMD_EXIT)) {
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
+
+        System.out.println("I hope to serve you again soon!");
     }
 }
