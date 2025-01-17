6a8689ec254e9e9e20e2cbf9ce92639b3789fabf
NhatMinh0208 <minhkhicon2468@gmail.com>
2025-01-17 21:13:11 +0800

    Implement greet and exit

--- src/main/java/Merylwb.java
+++ src/main/java/Merylwb.java
@@ -1,6 +1,8 @@
 public class Merylwb {
     public static void main(String[] args) {
         String name = "Merylwb";
-
+        System.out.println("Hello! I'm " + name + "!");
+        System.out.println("How can I help you today?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
