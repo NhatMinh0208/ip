7daaa99b083cf7707ecea5e5b4403027e0482b4b
NhatMinh0208 <minhkhicon2468@gmail.com>
2025-01-18 09:14:38 +0800

    Add task type Event

---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+public class Event extends Task {
+    private final String from;
+    private final String to;
+
+    public Event(String content, String from, String to) {
+        super(content);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + "(from: " + this.from + ", to: " + this.to + ")";
+    }
+}
