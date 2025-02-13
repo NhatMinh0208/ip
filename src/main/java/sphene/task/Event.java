330354428b1391cb8e4e86b3010b47be4c9b0c58
NhatMinh0208 <minhkhicon2468@gmail.com>
2025-02-13 10:25:21 +0800

    Fix capitalization on endTIme

--- src/main/java/sphene/task/Event.java
+++ src/main/java/sphene/task/Event.java
@@ -8,25 +8,25 @@ import java.time.format.DateTimeFormatter;
  */
 public class Event extends Task {
     private final LocalDateTime startTime;
-    private final LocalDateTime endTIme;
+    private final LocalDateTime endTime;
 
     /**
      * Creates a new event task.
      * @param content The content of the event.
      * @param startTime The event start time.
-     * @param endTIme The event end time.
+     * @param endTime The event end time.
      */
-    public Event(String content, LocalDateTime startTime, LocalDateTime endTIme) {
+    public Event(String content, LocalDateTime startTime, LocalDateTime endTime) {
         super(content);
         this.startTime = startTime;
-        this.endTIme = endTIme;
+        this.endTime = endTime;
     }
 
     @Override
     public String toString() {
         return "[E]" + super.toString()
                 + " (from: " + this.startTime.format(DateTimeFormatter.ofPattern("HH:mm dd MMM yyyy"))
-                + ", to: " + this.endTIme.format(DateTimeFormatter.ofPattern("HH:mm dd MMM yyyy"))
+                + ", to: " + this.endTime.format(DateTimeFormatter.ofPattern("HH:mm dd MMM yyyy"))
                 + ")";
     }
 
@@ -34,6 +34,6 @@ public class Event extends Task {
     public String serialize() {
         return "E" + "," + super.serialize()
                 + "," + this.startTime.format(DateTimeFormatter.ISO_DATE_TIME)
-                + "," + this.endTIme.format(DateTimeFormatter.ISO_DATE_TIME);
+                + "," + this.endTime.format(DateTimeFormatter.ISO_DATE_TIME);
     }
 }
